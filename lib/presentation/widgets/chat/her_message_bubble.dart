07581bcff6b9f0d5d209ffaa47e1ddc81790d39b
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-06-15 14:21:12 -0600

    feat: Add _ImageBubble component to display images in HerMessageBubble

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -23,10 +23,43 @@ class HerMessageBubble extends StatelessWidget {
             ),
           ),
         ),
-        const SizedBox(height: 10.0), // Space between messages
+        const SizedBox(height: 5.0), // Space between messages
 
         //TODO: Add image
+         _ImageBubble(),
+        const SizedBox(height: 10.0), // Space between messages
       ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: Image.network(
+        'https://picsum.photos/200/300',
+        width: size.width * 0.6, // Adjust width as needed
+        height: size.width * 0.6, // Adjust height as needed
+        fit: BoxFit.cover, // Adjust the image to cover the container
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child; // If loading is complete, return the child
+          return Center(
+            child: CircularProgressIndicator(
+              value: loadingProgress.expectedTotalBytes != null
+                  ? loadingProgress.cumulativeBytesLoaded /
+                      (loadingProgress.expectedTotalBytes ?? 1)
+                  : null,
+            ),
+          );
+        },
+      ),
+      
     );
   }
 }
