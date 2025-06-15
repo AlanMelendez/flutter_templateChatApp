7f7ecea3aa2db4840e534d4e39e7dd4cbe47c665
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-06-15 14:48:02 -0600

    feat: Enhance MessageFieldBox with text controller and focus management

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -7,6 +7,9 @@ class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = OutlineInputBorder(
       borderRadius: BorderRadius.circular(20.0),
       borderSide: BorderSide(color: const Color.fromARGB(255, 120, 216, 9), width: 1.0),
@@ -26,22 +29,34 @@ class MessageFieldBox extends StatelessWidget {
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
+
+            final message = textController.value.text;
             // Handle send action
-            print('Value of the message field: ${context.findRenderObject()?.toString()}');
+            print('Value of the message field: ${message}');
+
+            textController.clear(); // Clear the field after sending
           },
         ),
       );
 
     return TextFormField(
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         // Handle send action when the user submits the field
         print('Value to press onFieldSubmitted: $value');
+        textController.clear(); // Clear the field after sending
+        focusNode.requestFocus(); // Keep focus on the text field
       },
-      onChanged: (value) {
-        // Handle changes in the message field
-        print('Current value of the message field: $value');
+      onTapOutside: (event) {
+        // Handle tap outside the text field to unfocus
+        focusNode.unfocus();
       },
+      // onChanged: (value) {
+      //   // Handle changes in the message field
+      //   print('Current value of the message field: $value');
+      // },
     );
   }
 }
