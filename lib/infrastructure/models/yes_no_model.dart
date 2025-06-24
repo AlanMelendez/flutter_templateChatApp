eb7332aefcfeee17f71525dad130155eb2196b18
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-06-23 23:25:11 -0600

    refactor: Update YesNoModel to use final fields and improve JSON parsing with Quicktype

--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,20 +1,56 @@
+// To parse this JSON data, do
+//
+//     final yesNoModel = yesNoModelFromJson(jsonString);
+
+import 'dart:convert';
+
+//YesNoModel yesNoModelFromJson(String str) => YesNoModel.fromJson(json.decode(str));
+
+//String yesNoModelToJson(YesNoModel data) => json.encode(data.toJson());
+
 class YesNoModel {
-  String answer;
-  bool force;
-  String image;
+    final String answer;
+    final bool forced;
+    final String image;
 
-  YesNoModel({
-    required this.answer,
-    required this.force,
-    required this.image,
-  });
+    YesNoModel({
+        required this.answer,
+        required this.forced,
+        required this.image,
+    });
 
-  factory YesNoModel.fromJson(Map<String, dynamic> json)
-    => YesNoModel(
-      answer: json['answer'] ?? '',
-      force: json['force'] ?? false,
-      image: json['image'] ?? '',
+    factory YesNoModel.fromJson(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
     );
 
-
+    Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+    };
 }
+
+
+
+// class YesNoModel {
+//   String answer;
+//   bool force;
+//   String image;
+
+//   YesNoModel({
+//     required this.answer,
+//     required this.force,
+//     required this.image,
+//   });
+
+//   factory YesNoModel.fromJson(Map<String, dynamic> json)
+//     => YesNoModel(
+//       answer: json['answer'] ?? '',
+//       force: json['force'] ?? false,
+//       image: json['image'] ?? '',
+//     );
+
+
+// }
